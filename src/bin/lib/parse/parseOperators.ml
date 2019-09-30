c1b8e9e30bb924421bbc7d94be92843f6548bd79
YSawc <niceage6690@gmail.com>
2019-09-30 23:05:24 +0900

    spell

--- src/bin/lib/parse/parseOperators.ml
+++ src/bin/lib/parse/parseOperators.ml
@@ -1,5 +1,16 @@
 (* per: parse each operators with JSON of the argment *)
 
+(* use *)
+
+(* TODO: deal with absolutely path *)
+#use "../../../assets/operators.ml"
+
+(* func *)
+
+let rec parseOperator str = match str with
+    "" -> ""
+    str
+
 (* test *)
 
 test1 = parseOperator "Z plus Z" =
