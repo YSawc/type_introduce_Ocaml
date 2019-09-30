0fdd008d581f2add302e03cddb1b034bffd45588
YSawc <niceage6690@gmail.com>
2019-10-01 06:22:28 +0900

    small delete

--- src/bin/lib/contains/contains.ml
+++ src/bin/lib/contains/contains.ml
@@ -9,7 +9,6 @@
 let contains s1 s2 =
     let re = Str.regexp_string s2
     in
-        (* try ignore (Str.search_forward re s1 0); true *)
         try ignore (Str.search_forward re s1 0);
         if s2 = ""
         then false
