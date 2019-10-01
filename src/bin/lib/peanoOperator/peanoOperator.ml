ccfdf0606d7d96df4dcaadfb95c3d0d397a77869
YSawc <niceage6690@gmail.com>
2019-10-02 06:41:26 +0900

    add test__peanoOperator

---
+++ src/bin/lib/peanoOperator/peanoOperator.ml
@@ -0,0 +1,9 @@
+(* use *)
+
+#use "../../../assets/operators/operators.ml"
+(* test *)
+
+let test1 = peanoOperator "Z plus S(Z)" = "S(Z)"
+let test2 = peanoOperator "Z times S(Z)" = "Z"
+let test3 = peanoOperator "S(Z) times S(S(Z))" = "S(S(Z))"
+let test4 = peanoOperator "S(S(Z)) times S(S(Z))" = "S(S(S(S(Z))))"
