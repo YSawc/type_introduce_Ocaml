b04b615ec4a15bd6704dfec8c7e597173c1fef29
YSawc <niceage6690@gmail.com>
2019-10-02 21:51:02 +0900

    small fix

--- src/bin/lib/peanoOperator/parseParenMatch.ml
+++ src/bin/lib/peanoOperator/parseParenMatch.ml
@@ -4,10 +4,10 @@
 
 (* test *)
 
-let test1 = parseParenMatch "S" = false
 let test2 = parseParenMatch "S(S(S(Z)))" = true
 let test3 = parseParenMatch "S(S(Z)" = false
 let test4 = parseParenMatch "S(SZ)" = false
 let test5 = parseParenMatch "()S" = false
+let test1 = parseParenMatch "S" = false
 let test6 = parseParenMatch "Z" = true
 let test7 = parseParenMatch "(Z)" = false
