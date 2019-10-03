d226663abadfe3ae0fba2478bdbb9c7e74203da8
YSawc <niceage6690@gmail.com>
2019-10-03 22:08:55 +0900

    add func__parseFirst

--- src/bin/lib/peanoOperator/parseFirst.ml
+++ src/bin/lib/peanoOperator/parseFirst.ml
@@ -1,9 +1,23 @@
 (* func *)
 
+(* data__peano *)
+
+let peanosData = [
+    'Z';
+    'S';
+]
+
+let rec parseFirst str peanosData = match peanosData with
+    [] -> false
+    | first :: rest ->
+        if str . [0] = first
+        then true
+        else parseFirst str rest
+
 (* test *)
-let test1 = parseFirst "S" = true
-let test2 = parseFirst "S(S(Z))" = true
-let test3 = parseFirst "S)" = true
-let test4 = parseFirst "Z" = true
-let test5 = parseFirst "." = false
-let test6 = parseFirst "" = false
+let test1 = parseFirst "S" peanosData = true
+let test2 = parseFirst "S(S(Z))" peanosData = true
+let test3 = parseFirst "S)" peanosData = true
+let test4 = parseFirst "Z" peanosData = true
+let test5 = parseFirst "." peanosData = false
+let test6 = parseFirst "" peanosData = false
