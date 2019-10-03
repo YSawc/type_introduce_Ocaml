a453c13a6a8418ea890f74e9bfa48bdee08b0a29
YSawc <niceage6690@gmail.com>
2019-10-03 21:59:13 +0900

    small fix

--- src/bin/lib/peanoOperator/parseFirst.ml
+++ src/bin/lib/peanoOperator/parseFirst.ml
@@ -2,8 +2,8 @@
 
 (* test *)
 let test1 = parseFirst "S" = true
-let test1 = parseFirst "S(S(Z))" = true
-let test1 = parseFirst "S)" = true
-let test2 = parseFirst "Z" = true
-let test3 = parseFirst "." = false
-let test4 = parseFirst "" = false
+let test2 = parseFirst "S(S(Z))" = true
+let test3 = parseFirst "S)" = true
+let test4 = parseFirst "Z" = true
+let test5 = parseFirst "." = false
+let test6 = parseFirst "" = false
