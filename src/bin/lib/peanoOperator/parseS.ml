bc57d26723fc6ce32a8444dc627ebba41a89199a
YSawc <niceage6690@gmail.com>
2019-10-04 22:56:25 +0900

    add val

--- src/bin/lib/peanoOperator/parseS.ml
+++ src/bin/lib/peanoOperator/parseS.ml
@@ -1,6 +1,13 @@
 (* per: read S and chain word *)
 (* premise: read first word is S *)
 
+(* val *)
+
+let charBuf = Buffer.create 32
+let openParenBuf = Buffer.create 64
+let closeParenBuf = Buffer.create 64
+let parenBuf = Buffer.create 128
+
 (* test *)
 let test1 = parseS "S(S(Z))" = true
 let test2 = parseS "S(S(Z)" = false
