9a71e662511dac4f854b0f59e2e9cdb2988e995f
YSawc <niceage6690@gmail.com>
2019-10-04 22:58:18 +0900

    add function distribution of string

--- src/bin/lib/peanoOperator/parseS.ml
+++ src/bin/lib/peanoOperator/parseS.ml
@@ -8,6 +8,25 @@ let openParenBuf = Buffer.create 64
 let closeParenBuf = Buffer.create 64
 let parenBuf = Buffer.create 128
 
+(* func *)
+
+let parseS (str:string) =
+    for i = 0 to String.length str do
+            let (ittr:char) = str . [i] in
+            if ittr = 'S'
+            then
+                Buffer.add_char charBuf 'S'  (* append charPop *)
+            else if ittr = 'Z'
+            then
+                Buffer.add_char charBuf 'Z'  (* append charPop *)
+            else if ittr = '('
+            then
+                Buffer.add_char openParenBuf '('  (* append charPop *)
+            else if ittr = ')'
+            then
+                Buffer.add_char closeParenBuf ')'  (* append charPop *)
+                (* readChar idx-1 *)
+          done
 (* test *)
 let test1 = parseS "S(S(Z))" = true
 let test2 = parseS "S(S(Z)" = false
