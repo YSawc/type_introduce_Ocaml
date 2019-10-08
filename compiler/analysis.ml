cc5d6826c4b6f640695dbe5972bf3a34bebda36e
YSawc <niceage6690@gmail.com>
2019-10-08 22:35:23 +0900

    delete comment

--- compiler/analysis.ml
+++ compiler/analysis.ml
@@ -14,33 +14,7 @@ let void () = ()
 
 type parenType = Open_paren | Close_paren
 
-(* let parseParen (key:string) = match key with *)
-(*   | "Open"-> print_string "Open_paren" *)
-(*   | (Open_paren)  -> () *)
-(*   | "Close" -> print_string "Close_paren" *)
-(*   | _ -> raise @@ Invalid_argument "Error cause parseParen! Please fix argument" *)
-(*   | (Close_paren) -> () *)
-
 let parseParen parenType = match parenType with
-  | (Open_paren)  -> print_string ( "Close_paren" ^ "\n" )
-  | (Close_paren)  -> print_string ( "Close_paren" ^ "\n" )
-
-(* type peanoNum = Z | S *)
-
-(* let read_input (str:string) () = *)
-(*   let len = String.length str in *)
-(*   try *)
-(*     let i = 0 in *)
-(*     let rec read_token = function *)
-(*       | (current, chr) when current >= len -> void *)
-(*       | (current, chr) when chr = 'S' -> parseS; *)
-(*       | (current, chr) when chr = 'Z' -> parseZ; *)
-(*       | (current, chr) when chr = '(' -> parseParen "Open"; *)
-(*       | (current, chr) when chr = ')' -> parseParen "Close"; *)
-(*       | _ -> raise @@ Invalid_argument "Error cause with token " *)
-(*     in *)
-(*     read_token (i + 1, str . [i + 1]) *)
-(*   with Exit -> print_string "finished read_input" ;; *)
 
 let read_input (str:string) =
   let len = String.length str in
