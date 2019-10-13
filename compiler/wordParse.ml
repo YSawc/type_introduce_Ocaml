ffb7961815c9e6457f377fa63d3bd2489479e00b
YSawc <niceage6690@gmail.com>
2019-10-13 13:16:25 +0900

    add temporary functions function for parseWords

--- compiler/wordParse.ml
+++ compiler/wordParse.ml
@@ -1,7 +1,24 @@
-let parseWords (str:string) : string list =
-  Str.split (Str.regexp " " ) str
+type wordType =
+  | Peano
+  | Operation
 
+type operations =
+  | Plus
+  | Times
+  | Is
 
 let parseWords (words:string) : string list =
   Str.split (Str.regexp " " ) words
 
+let parsePeanoSyntax (peano:string) =
+  ()
+
+let parseSyntax = function
+  | Peano -> ()
+  | Operation -> ()
+
+let parseWord (word:string) =
+  match word . [0] with
+  | 'S' | 'Z' -> parseSyntax Peano
+  (* TODO: refactoring *)
+  | _ -> ()
