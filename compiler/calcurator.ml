0f50d35cde7baf2d725a251be0d5f466598c126f
YSawc <niceage6690@gmail.com>
2019-10-14 16:54:32 +0900

    add formaly functions

---
+++ compiler/calcurator.ml
@@ -0,0 +1,19 @@
+open WordParse
+
+let rec countSucessor (str:string) =
+  ()
+
+let exprPlus =
+  ()
+
+let exprTimes =
+  ()
+
+let exprIs (l:string) (r:string) =
+  ()
+
+let exprCalcurator (list:string) = function
+  | Plus -> ()
+  | Times -> ()
+
+let preExpr (list:string) =
