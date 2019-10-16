c5ef052226737206228fcf4adbdd403206a577b3
YSawc <niceage6690@gmail.com>
2019-10-16 21:29:46 +0900

    delete countSuccessor because of not definition of peano

--- compiler/calcurator.ml
+++ compiler/calcurator.ml
@@ -1,18 +1,5 @@
 open WordParse
 
-let rec countSuccessor (str:string) =
-  let nextIndexOfS = String.index str 'S'
-  in
-  try
-    let fillStr =
-      String.sub str (nextIndexOfS + 1) (String.length str)
-    in
-    ()
-    ;
-    fillStr |> countSuccessor
-  with Not_found ->
-    ()
-
 let exprPlus =
   ()
 
@@ -25,5 +12,3 @@ let exprIs (l:string) (r:string) =
 let exprCalcurator (list:string) = function
   | Plus -> ()
   | Times -> ()
-
-let preExpr (list:string) =
