e0cf466b43c2f48cf3260a293e62e15bcf3bdd4b
YSawc <niceage6690@gmail.com>
2019-11-12 21:54:01 +0900

    small change of style

--- compiler/inferenceChecker.ml
+++ compiler/inferenceChecker.ml
@@ -3,15 +3,9 @@ open PeanoDefinator
 open Calcurator
 
 let inferenceChecker = function
-  | (Plus, 'Z') ->
-    exprDefinator P_Zero
-  | (Plus, 'S') ->
-    exprDefinator P_Succ
-  | (Plus, _) ->
-    ()
-  | (Times, 'Z') ->
-    exprDefinator T_Zero
-  | (Times, 'S') ->
-    exprDefinator T_Succ
-  | (Times, _) ->
-    ()
+  | (Plus, 'Z') -> exprDefinator P_Zero
+  | (Plus, 'S') -> exprDefinator P_Succ
+  | (Plus, _) -> ()
+  | (Times, 'Z') -> exprDefinator T_Zero
+  | (Times, 'S') -> exprDefinator T_Succ
+  | (Times, _) ->()
